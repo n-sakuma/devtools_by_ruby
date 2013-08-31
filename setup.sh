3727d443927e110ffe2303dd7916bcc72796a2bf
Naohiro Sakuma <n.sakuma@everyleaf.com>
2013-08-31 22:04:57 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,17 @@
-for file in $(ls dotfiles); do
-  cp -b dotfiles/$file ~/.$file
+$( which bundle >/dev/null )
+if [ ! $? -eq 0 ]; then
+  echo "Not found 'bundle' ->  gem i bundler "
+  exit 1
+fi
+
+echo "bundle install..."
+bundle install
+
+echo "dotfiles copy... "
+for dotfile in $( ls dotfiles/* ); do
+  f=$(basename $dotfile)
+  cp -b $dotfile ~/.$f
 done
+
+# sleep 1
+echo "... Done"
